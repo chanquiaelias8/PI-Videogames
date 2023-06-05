a1670b3af75a4aa0b65303ddb8012cab023e4eed
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-05 18:49:11 -0300

    add routes platforms

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 // Importar todos los routers;
 const videogamesRouter = require('./videogamesRoutes.js');
 const genresRouter = require('./genresRoutes.js');
+const platformsRouter = require('./platformsRoutes.js');
 
 const router = Router();
 
 // Configurar los routers
 router.use('/videogames', videogamesRouter);
 router.use('/genres', genresRouter);
+router.use('/platforms', platformsRouter);
 
 
 module.exports = router;
