d27fbbbe9f202f40f921ac5ac16cbfef9d24c0aa
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-07 09:16:23 -0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import LandingPage from './views/LandingPage/LandingPage';
+import HomePage from './views/HomePage/HomePage';
+import DetailPage from './views/detailPage/DetailPage';
+import FormPage from './views/FormPage/FormPage';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/detail/:id" component={DetailPage}/>
+        <Route path="/createVideogame" component={FormPage}/>
+      </Switch>
     </div>
   );
 }
