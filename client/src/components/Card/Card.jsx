8b328b1af13fc82a3d49b96dd59222cf82fc3e01
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-10 12:15:38 -0300

    add changes

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import './Card.css'
 import { Link } from 'react-router-dom';
 
-export default function Card({ image, name, genres, id }) {
-
+export default function Card({id, image, name, genres}) { 
   return (
     <Link to={`/detail/${id}`} className="card">
-      <img src={'https://assets-prd.ignimgs.com/2021/12/30/36190303-image-7-1640880187142.png'} alt={name} />
+      <img src={`${image}`} alt={name} />
       <h3>{name}</h3>
-      <p>Género: {genres}</p>
+      <p>Género: {genres.map((genre, index) => index === genres.length-1?`${genre.name}.`:`${genre.name}, `)}</p>
     </Link>
   );
 }
