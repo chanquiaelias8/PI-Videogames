0225de2ebffaa7b339b3b88a9fdebbb1df4388ae
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-10 12:07:58 -0300

    add browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import LandingPage from './views/LandingPage/LandingPage';
 import HomePage from './views/HomePage/HomePage';
-import DetailPage from './views/detailPage/DetailPage';
+import DetailPage from './views/DetailPage/DetailPage';
 import FormPage from './views/FormPage/FormPage';
 
 function App() {
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/home" component={HomePage} />
-        <Route path="/detail/:id" component={DetailPage}/>
-        <Route path="/createVideogame" component={FormPage}/>
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={HomePage} />
+          <Route path="/detail/:id" component={DetailPage}/>
+          <Route path="/createVideogame" component={FormPage}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
