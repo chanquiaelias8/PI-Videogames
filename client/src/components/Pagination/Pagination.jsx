9cc43fc09078de7e3a210d8d30d7d09a6426aeb2
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-10 12:21:33 -0300

    add pagination

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import './Pagination.css'; // Importa el archivo CSS externo
 
-export default function Pagination() {
-  return (
-    <div>Componente de Paginación</div>
-  )
-}
+import React from 'react';
+
+export default function Pagination({ currentPage, totalPages, onPageChange }){
+  const handleClick = (page) => {
+    if (onPageChange) {
+      onPageChange(page);
+    }
+  };
+
+  const renderPaginationItems = () => {
+    const items = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      const isActive = i === currentPage;
+      items.push(
+        <div
+          key={i}
+          className={`pagination-item ${isActive ? 'active' : ''}`}
+          onClick={() => handleClick(i)}
+        >
+          {i}
+        </div>
+      );
+    }
+
+    return items;
+  };
+
+  return <div className="pagination">{renderPaginationItems()}</div>;
+};
