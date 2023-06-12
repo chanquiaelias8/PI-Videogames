642cd428da346e49ada1ea0a31da5a7f9eef6264
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-12 17:16:11 -0300

    import useParams, import getDetail and CleanId from actions, add ternary operator for detail container

--- client/src/views/detailPage/DetailPage.jsx
+++ client/src/views/detailPage/DetailPage.jsx
@@ -1,15 +1,14 @@
 import './DetailPage.css'
 import React, { useEffect, useState } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { useParams } from 'react-router-dom/cjs/react-router-dom';
+import { useParams } from 'react-router-dom';
 
 // import components
 import Navbar from '../../components/Navbar/Navbar';
 import Loader from '../../components/Loader/Loader';
 
-
 // import actions
-import {get_Detail, clean_ID} from '../../redux/actions/index'
+import { get_Detail, clean_ID } from '../../redux/actions/index'
 
 export default function DetailPage() {
 
@@ -24,7 +23,7 @@ export default function DetailPage() {
 			setLoading(true);
       dispatch(clean_ID());
 		};
-  },[id])
+  },[dispatch, id])
 
   console.log(detail);
 
@@ -32,13 +31,26 @@ export default function DetailPage() {
     <>
     <Navbar/>
     {loading? (<Loader/>): (
-      <div className='container'>
+      typeof detail.id === "number"
+      ?<div className='container'>
       <h2>Detalle del juego</h2>
         <div>
           <h3>{detail.name}</h3>
           <img src={detail.background_image} alt={detail.name} />
           <p>Plataformas: {detail.platforms.map((e, index) => index === detail.platforms.length-1?`${e.platform.name}.`:`${e.platform.name}, ` )}</p>
           <p>Descripción: {detail.description_raw}</p>
+          <p>Fecha de lanzamiento: {detail.released}</p>
+          <p>Rating: {detail.rating}</p>
+          <p>Géneros: {detail.genres.map((e, index) =>detail.genres.length-1 === index? `${e.name}.` : `${e.name}, `)}</p>
+        </div>
+      </div>
+      :<div className='container'>
+      <h2>Detalle del juego</h2>
+        <div>
+          <h3>{}</h3>
+          <img src={detail.background_image} alt={detail.name} />
+          <p>Plataformas: {detail.platforms.map((e, index) => index === detail.platforms.length-1?`${e.name }.`:`${e.name}, ` )}</p>
+          <p>Descripción: {detail.description}</p>
           <p>Fecha de lanzamiento: {detail.released}</p>
           <p>Rating: {detail.rating}</p>
           <p>Géneros: {detail.genres.map((e, index) =>detail.genres.length-1 === index? `${e.name}.` : `${e.name}, `)}</p>
