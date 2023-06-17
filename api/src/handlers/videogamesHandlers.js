5362504aa1e8fa8666ee0103cb686a4e992b11f9
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-17 17:50:36 -0300

    return videogames with name

--- api/src/handlers/videogamesHandlers.js
+++ api/src/handlers/videogamesHandlers.js
@@ -4,7 +4,12 @@ const {
     get_Videogame_ById } = require('../controllers/videogamesControllers.js')
 
 const Get_Videogames_Handler = async (req, res) => {
+    const {name} = req.query;
     try {
+        if(name){
+            const response = await get_All_Videogames(name);
+            return res.status(200).json(response);
+        }
         const response = await get_All_Videogames();
         return res.status(200).json(response)
     } catch (error) {
