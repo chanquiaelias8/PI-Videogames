632882b9d5136342b8d948214935d256be851451
Elias Chanquia <chanquiaelias8@gmail.com>
2023-06-17 17:58:41 -0300

    change navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,45 +1,75 @@
 import './Navbar.css';
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import Modal from '../Modal/Modal';
 
-const Navbar = () => {
+const Navbar = ({ handleSubmit, handleResetSearch, isSearching }) => {
   const history = useHistory();
-  const [searchValue, setSearchValue] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [textButton, setTextButton] = useState('Search');
+  const [showModal, setShowModal] = useState(false);
 
-  const handleSearchChange = (event) => {
-    setSearchValue(event.target.value);
+  function handleInputChange(event) {
+    setSearchInput(event.target.value);
   }
 
-  const handleSearchSubmit = (event) => {
+  function handleFormSubmit(event) {
     event.preventDefault();
-    // Aquí puedes agregar la lógica para manejar la búsqueda, por ejemplo, hacer una solicitud a la API o realizar alguna acción específica.
-    console.log('Búsqueda realizada:', searchValue);
+    if (textButton === 'Search') {
+      if (searchInput === '') {
+        setShowModal(true);
+      }else{
+        handleSubmit(searchInput);
+        setTextButton('Reset Search')
+      }
+    }else if(textButton ==='Reset Search'){
+      handleResetSearch();
+      setTextButton('Search');
+    }
   }
 
+  function handleCloseModal() {
+    setShowModal(false);
+  }
 
   return (
     <nav>
       <div className="navbar">
         <h1>Nombre de tu Aplicación</h1>
         <div className="nav-buttons">
-          <form className="search-form" onSubmit={handleSearchSubmit}>
+          <form className="search-form" onSubmit={handleFormSubmit}>
             <input
-              type="text"
-              placeholder="Buscar..."
-              value={searchValue}
-              onChange={handleSearchChange}
+              type="search"
+              placeholder="Search by name"
+              value={searchInput}
+              onChange={handleInputChange}
             />
-            <button type="submit">Buscar</button>
+            <button type="submit">
+              {isSearching ? textButton : textButton}
+            </button>
           </form>
         </div>
-        {
-          history.location.pathname === "/home"
-            ?<Link to="/createVideogame" className="nav-button">Create Videogame</Link>
-            :<Link to="/home" className="nav-button">Home</Link>
-        }
+        {history.location.pathname === '/home' ? (
+          <Link to="/createVideogame" className="nav-button">
+            Create Videogame
+          </Link>
+        ) : (
+          <Link to="/home" className="nav-button">
+            Home
+          </Link>
+        )}
       </div>
+
+      {showModal && (
+        <Modal
+          title="Input vacio"
+          onClose={handleCloseModal}
+        >
+          <p>El campo de búsqueda está vacío. Por favor, ingresa un valor antes de buscar.</p>
+        </Modal>
+      )}
     </nav>
   );
-}
+};
 
 export default Navbar;
